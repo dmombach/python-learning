98ae651a462588db0044dfe051b287921c697185
DanMombach <dan.mombach@oracle.com>
2026-04-02 11:31:47 -0400

    Initial commit.

---
+++ main.py
@@ -0,0 +1,46 @@
+print("Hello Dan, welcome to Python!")
+
+# >>> Variables
+name = "Dan"
+age = 45
+
+# >>> Types
+# • int
+# • float
+# • str
+# • bool
+# • list
+# • dict
+# • tuple
+
+
+# >>> Functions
+def greet(name):
+    return f"Hello {name}"
+
+
+# >>> Conditionals
+if age > 18:
+    print("Adult")
+
+# >>> Loops
+for i in range(5):
+    print(i)
+
+# >>> Modules
+# import math
+
+# >>> Packages (pip)
+# pip install requests
+
+# >>> Virtual environments
+# You already created one.
+
+# >>> File I/O
+# with open("data.txt") as f:
+#     print(f.read())
+
+# >>> Classes (Python’s simple OOP)
+# class Person:
+#     def __init__(self, name):
+#         self.name = name
